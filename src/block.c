8a28acc5cbcca2f78daa7e67e8a151f5b38cbfc0
Ryan Mendivil <ryan@nullreff.net>
2014-04-10 17:37:03 -0800

    Update hashing method to be more uniform

--- src/block.c
+++ src/block.c
@@ -62,8 +62,10 @@ int location_equals(Location l1, Location l2)
     return l1.x == l2.x && l1.y == l2.y && l1.z == l2.z;
 }
 
+// I chose 101 because it kind of looks like two redstone torches :)
+#define MAGIC_HASH_NUBER 101
 int location_hash(Location loc, int max)
 {
-    // TODO: Use a better hashing method
-    return abs(loc.x ^ loc.y ^ loc.z) % max;
+    // If you know of a better hashing method, feel free to alter this one
+    return abs((loc.x * MAGIC_HASH_NUBER + loc.y) * MAGIC_HASH_NUBER + loc.z) % max;
 }
