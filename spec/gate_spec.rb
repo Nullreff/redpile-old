89d417173e63b24915af20dc844aebd36ab02a01
Ryan Mendivil <ryan@nullreff.net>
2014-06-23 14:34:31 -0800

    Spelling

--- spec/gate_spec.rb
+++ spec/gate_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 include Helpers
 
-# More complext logic gate tests.  Taken from:
+# More complex logic gate tests.  Taken from:
 # http://minecraft.gamepedia.com/Tutorials/Basic_Logic_Gates
 
 describe 'Logic' do
