61200f09ca56a617ce4800e73413b91ab6586c18
Ryan Mendivil <ryan@nullreff.net>
2014-06-30 16:03:11 -0800

    Remove GA script

--- docs/generate.rb
+++ docs/generate.rb
@@ -22,14 +22,6 @@ __END__
     <meta charset="utf-8">
     <title><%= @title %></title>
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
-    <script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-      ga('create', 'UA-27731627-5', 'auto');
-      ga('send', 'pageview');
-    </script>
   </head>
   <body>
     <div class="container">
