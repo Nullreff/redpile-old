7a4b888b44882213ce961dda0a30dfe7bedba8ae
Ryan Mendivil <ryan@nullreff.net>
2014-07-21 03:54:17 -0800

    Refactor switch_spec to read named fields

--- spec/switch_spec.rb
+++ spec/switch_spec.rb
@@ -4,59 +4,65 @@ include Helpers
 describe 'Switch' do
   context 'turned on' do
     it 'powers a conductor behind it' do
-      run(
+      result = run(
         'SET 0 0 0 SWITCH direction:UP state:1',
         'SET 0 -1 0 CONDUCTOR',
         'TICK',
         'GET 0 -1 0'
-      ).should =~ /^\(0,-1,0\) CONDUCTOR 15$/
+      )
+      contains_node?(result, 0, -1, 0, 'CONDUCTOR', power: 15)
     end
 
     it 'does not power a conductor to the side' do
-      run(
+      result = run(
         'SET 0 0 0 SWITCH direction:UP state:1',
         'SET 1 0 0 CONDUCTOR',
         'TICK',
         'GET 1 0 0'
-      ).should =~ /^\(1,0,0\) CONDUCTOR 0$/
+      )
+      contains_node?(result, 1, 0, 0, 'CONDUCTOR', power: 0)
     end
 
     it 'powers a wire to the side of it' do
-      run(
+      result = run(
         'SET 0 0 0 SWITCH direction:UP state:1',
         'SET 1 0 0 WIRE',
         'TICK',
         'GET 1 0 0'
-      ).should =~ /^\(1,0,0\) WIRE 15$/
+      )
+      contains_node?(result, 1, 0, 0, 'WIRE', power: 15)
     end
   end
 
   context 'turned off' do
     it 'does not power a conductor behind it' do
-      run(
+      result = run(
         'SET 0 0 0 SWITCH direction:UP state:0',
         'SET 0 -1 0 CONDUCTOR',
         'TICK',
         'GET 0 -1 0'
-      ).should =~ /^\(0,-1,0\) CONDUCTOR 0$/
+      )
+      contains_node?(result, 0, -1, 0, 'CONDUCTOR', power: 0)
     end
 
     it 'does not power a conductor to the side' do
-      run(
+      result = run(
         'SET 0 0 0 SWITCH direction:UP state:0',
         'SET 1 0 0 CONDUCTOR',
         'TICK',
         'GET 1 0 0'
-      ).should =~ /^\(1,0,0\) CONDUCTOR 0$/
+      )
+      contains_node?(result, 1, 0, 0, 'CONDUCTOR', power: 0)
     end
 
     it 'does not power a wire to the side of it' do
-      run(
+      result = run(
         'SET 0 0 0 SWITCH direction:UP state:0',
         'SET 1 0 0 WIRE',
         'TICK',
         'GET 1 0 0'
-      ).should =~ /^\(1,0,0\) WIRE 0$/
+      )
+      contains_node?(result, 1, 0, 0, 'WIRE', power: 0)
     end
   end
 end
