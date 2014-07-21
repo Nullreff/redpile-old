7c1ef380e241ceb7353039e84a2080434f1f1b11
Ryan Mendivil <ryan@nullreff.net>
2014-07-21 03:15:24 -0800

    Refactor wire_spec to read named fields

--- spec/wire_spec.rb
+++ spec/wire_spec.rb
@@ -13,7 +13,7 @@ describe 'Wire' do
   end
 
   it 'propigates power to the side and up' do
-    run(
+    result = run(
       'SET 0 0 0 SWITCH direction:UP state:1',
       'SET 0 0 1 WIRE',
       'SET 0 0 2 CONDUCTOR',
@@ -23,7 +23,7 @@ describe 'Wire' do
   end
 
   it 'does not propigate to the side and up when a block is on top' do
-    run(
+    result = run(
       'SET 0 0 0 SWITCH direction:UP state:1',
       'SET 0 0 1 WIRE',
       'SET 0 0 2 CONDUCTOR',
@@ -31,7 +31,8 @@ describe 'Wire' do
       'SET 0 1 1 CONDUCTOR',
       'TICK',
       'GET 0 1 2'
-    ).should =~ /^\(0,1,2\) WIRE 0$/
+    )
+    contains_node?(result, 0, 1, 2, 'WIRE', power: 0)
   end
 
   it 'powers an adjacent conductor' do
@@ -52,7 +53,7 @@ describe 'Wire' do
       'TICK',
       'GET 0 0 2'
     )
-    result.should =~ /^\(0,0,2\) CONDUCTOR 0$/
+    contains_node?(result, 0, 0, 2, 'CONDUCTOR', power: 0)
   end
 
   it 'diverts power from a condutor when a wire is on the right' do
@@ -64,7 +65,7 @@ describe 'Wire' do
       'TICK',
       'GET 0 0 2'
     )
-    result.should =~ /^\(0,0,2\) CONDUCTOR 0$/
+    contains_node?(result, 0, 0, 2, 'CONDUCTOR', power: 0)
   end
 
   it 'diverts power from a condutor when a wire is on both sides' do
@@ -77,7 +78,7 @@ describe 'Wire' do
        'TICK',
       'GET 0 0 2'
     )
-    result.should =~ /^\(0,0,2\) CONDUCTOR 0$/
+    contains_node?(result, 0, 0, 2, 'CONDUCTOR', power: 0)
   end
 
   it 'does not divert power from a conductor if there is a wire on top' do
@@ -91,7 +92,7 @@ describe 'Wire' do
        'TICK',
        'GET 0 0 2'
     )
-    result.should =~ /^\(0,0,2\) CONDUCTOR 14$/
+    contains_node?(result, 0, 0, 2, 'CONDUCTOR', power: 14)
   end
 
   it 'propigates power to a conductor underneath wire' do
