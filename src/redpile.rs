513eb63a261a6e80ad66da1fabe51bfa87f086bb
Ryan Mendivil <ryan@nullreff.net>
2015-02-13 20:45:03 -0800

    Update to work on the latest version of rust

--- src/redpile.rs
+++ src/redpile.rs
@@ -1,18 +1,22 @@
+#![feature(libc)]
+#![feature(env)]
+#![feature(std_misc)]
+
 extern crate libc;
 use libc::{c_char, c_int};
-use std::os;
+use std::env::args;
 use std::ffi::CString;
-use std::iter;
 
 #[link(name = "lua", kind= "static")]
 #[link(name = "linenoise", kind= "static")]
 #[link(name = "redpile-core", kind= "static")]
 extern {
-    fn redpile_run(argc: c_int, argv: *const CString) -> c_int;
+    fn redpile_run(argc: c_int, argv: *const *const c_char) -> c_int;
 }
 
 fn main() {
-    let args = os::args().iter().map(|&:arg| CString::from_slice(arg.as_bytes())).collect::<Vec<CString>>();
+    let args = args().map(|&:arg| CString::from_slice(arg.as_bytes()).as_ptr())
+                     .collect::<Vec<*const c_char>>();
     unsafe {
         redpile_run(args.len() as c_int, args.as_ptr());
     }
