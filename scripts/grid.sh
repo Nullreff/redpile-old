adb9202d8bd1807f0981ca42bd438e4367b5d1fb
Ryan Mendivil <ryan@nullreff.net>
2016-12-23 02:23:09 -0800

    Move params for grid.sh to variables

--- scripts/grid.sh
+++ scripts/grid.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-COMMANDS="NODE -40..40,0,-40..40 wire\nNODE -20..20%10,0,-20..20%10 torch direction:UP\nTICKQ 2\nFIELD -40..40,0,-40..40 power"
-OUTPUT=`echo -e $COMMANDS | ./build/src/redpile conf/redstone.lua | tr ',' ' '`
+WIRE=100
+TORCH=80
+COMMANDS="NODE -$WIRE..$WIRE,0,-$WIRE..$WIRE wire\nNODE -$WIRE..$WIRE%10,0,-$WIRE..$WIRE%10 torch direction:UP\nTICKQ 2\nFIELD -40..40,0,-40..40 power"
+OUTPUT=`echo -e $COMMANDS | ./build/src/redpile conf/redstone.lua | tr ',' ' ' | sed 's/nil/0/'`
 
 echo "
 set term png
